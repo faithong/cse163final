39edfa2a60473a3610f8cefc4335d90bf6794342
faithong <faithong02@gmail.com>
2022-03-09 12:24:38 -0800

    added visualizations for pair_type and gene_class

--- final_project.py
+++ final_project.py
@@ -19,18 +19,16 @@ all_races = all_races.loc[:, all_races.columns != 'gene_id']
 st.write(all_races)
 
 # visualization for gene_biotype
-# gene_biotype_graph = all_races[]
 gene_biotype_graph = sns.catplot(data=all_races, x="gene_biotype", kind="count", ci=None)
-st.write(gene_biotype_graph)
-# gene_biotype_graph.set_title("Gene Biotype")
-# gene_biotype_graph.set_ylabel("Number")
-
-# plt.savefig('bar_chart_high_school.png', bbox_inches='tight')
+st.pyplot(gene_biotype_graph)
 
 
 # visualization for pair type
+pair_type_graph = sns.catplot(data=all_races, x="pair_type", kind="count", ci=None)
+st.pyplot(pair_type_graph)
 
 
 # visualization for gene class
-
+gene_class_graph = sns.catplot(data=all_races, x="gene_class", kind="count", ci=None)
+st.pyplot(gene_class_graph)
 
